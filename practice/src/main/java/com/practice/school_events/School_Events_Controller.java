d2657ca0c1ee6310207fcfeb9e94052f36773e49
costar011 <51503128+costar011@users.noreply.github.com>
2019-11-14 19:38:54 +0900

    school

--- practice/src/main/java/com/practice/school_events/School_Events_Controller.java
+++ practice/src/main/java/com/practice/school_events/School_Events_Controller.java
@@ -7,6 +7,7 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.practice.model.SchoolEventVO;
 
@@ -15,17 +16,35 @@ import com.practice.model.SchoolEventVO;
 public class School_Events_Controller {
 	
 	@Resource(name = "school_events_Service")
-	School_Events_Service ssService;
+	School_Events_Service ss;
 
 	@RequestMapping("/schoolPage")
 	public String schoolevents(Model model) {
 		
 		System.out.println("111111111");
 		
-		List<SchoolEventVO> list = ssService.sleectAllevent();
+		List<SchoolEventVO> list = ss.sleectAllevent();
 		
 		model.addAttribute("eventList", list);
 		
 		return "schooleventsPage";
 	}
+	
+	
+	@RequestMapping("/eventInsert")
+	public String schoolevents(@RequestParam("title")String title
+							   ,@RequestParam("written")String written) {
+		
+		SchoolEventVO vo = new SchoolEventVO();
+		
+		vo.setTitle(title);
+		vo.setWrittin(written);
+		
+		ss.sleectAllevent();
+		
+		return "schooleventsPage";
+		
+	}
+	
+	
 }
--- practice/src/main/webapp/WEB-INF/views/school_events/school_events.jsp
+++ practice/src/main/webapp/WEB-INF/views/school_events/school_events.jsp
@@ -94,7 +94,7 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal">&times;</button>
-						<h4 class="modal-title">Modal Header</h4>
+						<h4 class="modal-title">Would you like to register?</h4>
 					</div>
 					<div class="modal-body">
 						<input type="text" class="modal-input" placeholder="title" /> 
